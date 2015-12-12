7cc8fedd01fb59aefd5a0cc37d4bf7dd320be424
James Black <james@james-black.com>
2015-12-11 22:20:32 -0700

    Adding the battleManager, and writing some initial init stuff for it

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BattleManager : MonoBehaviour {
+
+	// We need to keep track of all the combatants
+	// Use 2 lists of Dictionarys
+
+	public List<Dictionary<string, object>> TeamA = new List<Dictionary<string, object>>();
+
+	public List<Dictionary<string, object>> TeamB = new List<Dictionary<string, object>>();
+
+	// Use this for initialization
+	void Start () {
+		foreach (Transform team in transform) {
+			foreach ( Transform unit in team ) {
+				GameObject unitObject = unit.gameObject;
+				// This is where we would grab the script of the unit.
+				if (team.name == "TeamA") {
+					TeamA.Add(new Dictionary<string, object>() {
+						{ "object", unitObject }
+					});
+				} else if(team.name == "TeamB") {
+					TeamB.Add(new Dictionary<string, object>() {
+						{ "object", unitObject }
+					});
+				}
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
